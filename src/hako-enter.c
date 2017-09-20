d0d93eb57a73862eaf655fe5373d6b5835ab2f87
Bach Le <bach@bullno1.com>
2017-09-20 16:10:40 +0800

    Add --fork to hako-enter

--- src/hako-enter.c
+++ src/hako-enter.c
@@ -6,9 +6,11 @@
 #include <assert.h>
 #include <sched.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define OPTPARSE_IMPLEMENTATION
 #define OPTPARSE_API static
 #include "optparse.h"
@@ -19,6 +21,59 @@
 
 #define PROG_NAME "hako-enter"
 #define quit(code) exit_code = code; goto quit;
+
+static bool
+enter_sandbox(const char* pid)
+{
+	bool exit_code = true;
+	DIR* dir = NULL;
+	char ns_dir[256]; // long enough to hold path to namespace
+
+	if(snprintf(ns_dir, sizeof(ns_dir), "/proc/%s/ns", pid) > (int)sizeof(ns_dir))
+	{
+		fprintf(stderr, "Invalid pid\n");
+		quit(false);
+	}
+
+	if(chdir(ns_dir) == -1)
+	{
+		fprintf(stderr, "chdir(\"%s\") failed: %s\n", ns_dir, strerror(errno));
+		quit(false);
+	}
+
+	dir = opendir(".");
+	if(dir == NULL)
+	{
+		perror("Could not examine sandbox");
+		quit(false);
+	}
+
+	struct dirent* dirent;
+	while((dirent = readdir(dir)) != NULL)
+	{
+		if(dirent->d_type != DT_LNK) { continue; }
+
+		int ns = open(dirent->d_name, O_RDONLY);
+		if(ns < 0 && errno != ENOENT)
+		{
+			fprintf(stderr, "Could not open %s: %s\n", dirent->d_name, strerror(errno));
+			quit(EXIT_FAILURE);
+		}
+
+		if(ns > 0 && setns(ns, 0) == -1)
+		{
+			fprintf(stderr, "Could not setns %s: %s\n", dirent->d_name, strerror(errno));
+			quit(EXIT_FAILURE);
+		}
+
+		close(ns);
+	}
+
+quit:
+	if(dir != NULL) { closedir(dir); }
+
+	return exit_code;
+}
 
 int
 main(int argc, char* argv[])
@@ -41,8 +96,7 @@ main(int argc, char* argv[])
 	const char* usage = "Usage: " PROG_NAME " [options] <pid> [--] [command] [args]";
 
 	int option;
-	DIR* dir = NULL;
-	char ns_dir[256]; // long enough to hold path to namespace
+	bool fork_before_exec = false;
 	struct optparse options;
 	struct run_ctx_s run_ctx;
 
@@ -56,6 +110,9 @@ main(int argc, char* argv[])
 			case 'h':
 				optparse_help(usage, opts, help);
 				quit(EXIT_SUCCESS);
+				break;
+			case 'f':
+				fork_before_exec = true;
 				break;
 			CASE_RUN_OPT:
 				if(!parse_run_option(&run_ctx, PROG_NAME, option, options.optarg))
@@ -82,53 +139,46 @@ main(int argc, char* argv[])
 		quit(EXIT_FAILURE);
 	}
 
-	if(snprintf(ns_dir, sizeof(ns_dir), "/proc/%s/ns", pid) > (int)sizeof(ns_dir))
-	{
-		fprintf(stderr, "Invalid pid\n");
-		quit(EXIT_FAILURE);
-	}
+	if(!enter_sandbox(pid)) { quit(EXIT_FAILURE); }
 
-	if(chdir(ns_dir) == -1)
+	if(fork_before_exec)
 	{
-		fprintf(stderr, "chdir(\"%s\") failed: %s\n", ns_dir, strerror(errno));
-		quit(EXIT_FAILURE);
-	}
-
-	dir = opendir(".");
-	if(dir == NULL)
-	{
-		perror("Could not examine sandbox");
-		quit(EXIT_FAILURE);
-	}
-
-	struct dirent* dirent;
-	while((dirent = readdir(dir)) != NULL)
-	{
-		if(dirent->d_type != DT_LNK) { continue; }
-
-		int ns = open(dirent->d_name, O_RDONLY);
-		if(ns < 0 && errno != ENOENT)
+		pid_t child = vfork();
+		if(child == -1)
 		{
-			fprintf(stderr, "Could not open %s: %s\n", dirent->d_name, strerror(errno));
+			perror("vfork() failed");
 			quit(EXIT_FAILURE);
 		}
-
-		if(ns > 0 && setns(ns, 0) == -1)
+		else if(child == 0) // child
 		{
-			fprintf(stderr, "Could not setns %s: %s\n", dirent->d_name, strerror(errno));
-			quit(EXIT_FAILURE);
+			if(prctl(PR_SET_PDEATHSIG, SIGKILL, 0, 0, 0) == -1)
+			{
+				perror("Could not set parent death signal");
+				exit(EXIT_FAILURE);
+			}
+
+			if(!execute_run_ctx(&run_ctx)) { exit(EXIT_FAILURE); }
+
+			exit(EXIT_SUCCESS);// unreachable
 		}
+		else // parent
+		{
+			int status;
+			errno = 0;
+			while(waitpid(child, &status, 0) != child && errno == EINTR) {}
 
-		close(ns);
+			quit(
+				WIFEXITED(status) ?
+				WEXITSTATUS(status) : (128 + WTERMSIG(status))
+			);
+		}
 	}
-
-	if(!execute_run_ctx(&run_ctx))
+	else
 	{
-		quit(EXIT_FAILURE);
+		if(!execute_run_ctx(&run_ctx)) { quit(EXIT_FAILURE); }
 	}
 quit:
 	cleanup_run_ctx(&run_ctx);
-	if(dir != NULL) { closedir(dir); }
 
 	return exit_code;
 }
